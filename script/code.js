7115282b3726b37d6a833511e5f72a28ad39ae6b
Que Mnqika <qaqambilemnqika23@gmail.com>
2023-05-20 17:31:39 +0200

    js add

--- script/code.js
+++ script/code.js
@@ -1,3 +1,63 @@
-function screens(input) {
-    document.querySelector("#screen").input += input
+//reference from J Mukanya
+function screen(value) {
+    document.querySelector('#screen').value += value
 }
+
+document.querySelector('#delete').addEventListener('click', 
+    (
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    ) => {
+        let results = document.querySelector('#screen').value;
+        results = results.slice(0, -1);
+    })
