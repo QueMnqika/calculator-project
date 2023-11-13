6bd6bc84f1719961012a3f337622e476fd45d3c0
Que Mnqika <qaqambilemnqika23@gmail.com>
2023-11-13 17:12:51 +0200

    17:12 push

--- js/script.js
+++ js/script.js
@@ -1,31 +1,40 @@
 let input = document.querySelector('#userInput')
 let clearBtn = document.querySelector('#top-left')
 let deleteBtn = document.querySelector('#delete')
-let multiply = document.querySelector('#top-right')
-let add = document.querySelector('#add')
-let minus = document.querySelector('#minus')
-let divide = document.querySelector('#divide')
 let equalBtn = document.querySelector('#bottom-right')
+let btnNumbOps = document.querySelectorAll('[data-valueOperand]')
+let operands = ['+', '-', '*', '/', '.']
 
-
-function userInput(value) {
+ let uInput = (value) => {
     input.value += value
 }
 
-function clear(value) {
+let clear = (value) => {
     input.value = ''
 }
 
-clearBtn.addEventListener('click', () => {clear(value)})
+clearBtn.addEventListener('click', () => {
+    clear()
+})
 
-function deleting(value) {
+let deleting = (value) => {
     input.value = input.value.slice(0, -1)
 }
 
-deleteBtn.addEventListener('click', deleting())
+deleteBtn.addEventListener('click',() => {
+    deleting()
+})
 
-function equal(value) {
-    input.value = eval(input.value)
-}
 
-equalBtn.addEventListener('click', equal())
+equalBtn.addEventListener('click',() => {
+    let result = eval(input.value)
+    input.value = result
+})
+
+btnNumbOps.forEach((numbOps)=>{
+    numbOps.addEventListener('click', function(){
+        document.querySelector("#userInput").value += numbOps.value
+    })
+})
+
+
